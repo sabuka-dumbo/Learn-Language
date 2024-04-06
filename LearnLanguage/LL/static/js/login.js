ce29f830a5dfb55049d8804dfb5086366ba1e969
Sabuka-dumbo <anonym.sas@proton.me>
2024-04-06 09:54:44 +0000

    Apr 6, 2024, 1:54 PM

--- LearnLanguage/LL/static/js/login.js
+++ LearnLanguage/LL/static/js/login.js
@@ -3,11 +3,24 @@ const email_placeholder = document.getElementById("email-placeholder");
 
 email_input.addEventListener("focus", function() {
     email_placeholder.style.animation = "email-animation 0.4s ease";
+    email_input.style.outline = "black 1px solid";
 
-    email_input.addEventListener("animationend", function() {
+    email_placeholder.addEventListener("animationend", function() {
         email_placeholder.style.animation = '';
         email_placeholder.style.top = "-1.2vw";
         email_placeholder.style.left = "20%";
         email_placeholder.style.fontSize = "1.5vw";
+    })
+})
+
+email_input.addEventListener("focusout", function() {
+    email_placeholder.style.animation = "email-animation-2 0.4s ease";
+    email_input.style.outline = '';
+
+    email_placeholder.addEventListener("animationend", function() {
+        email_placeholder.style.animation = '';
+        email_placeholder.style.top = '';
+        email_placeholder.style.left = '';
+        email_placeholder.style.fontSize = '';
     })
 })
