6e6da6fe21a4dc80d1e4f2b9f0c27da00f9cde82
Sabuka-dumbo <anonym.sas@proton.me>
2024-04-13 14:31:37 +0000

    Apr 13, 2024, 6:31 PM

--- LearnLanguage/LL/static/js/login.js
+++ LearnLanguage/LL/static/js/login.js
@@ -1,31 +1,59 @@
 const email = document.getElementById("email");
 const email_placeholder = document.getElementById("email-placeholder");
 
-email.addEventListener("focus", function() {
-    email_placeholder.style.animation = "input-placeholder-animation 0.4s ease";
-
-    email_placeholder.addEventListener("animationend", function() {
-        email_placeholder.style.animation = '';
-        email_placeholder.style.top = "-100%";
-        email_placeholder.style.zIndex = "6";
-        email_placeholder.style.fontSize = "1.2vw";
-        email_placeholder.style.transform = "translateY(100%)";
-    })
-})
-
-email.addEventListener("focusout", function() {
-    if (email.value.length == 0) {
-        email_placeholder.style.animation = "input-placeholder-animation-2 0.4s ease";
+if (window.innerWidth < 680) {
+    email.addEventListener("focus", function() {
+        email_placeholder.style.animation = "input-placeholder-animation 0.4s ease";
 
         email_placeholder.addEventListener("animationend", function() {
             email_placeholder.style.animation = '';
-            email_placeholder.style.top = "50%";
-            email_placeholder.style.zIndex = "5";
-            email_placeholder.style.fontSize = "1.5vw";
-            email_placeholder.style.transform = "translateY(-50%)";
+            email_placeholder.style.top = "-100%";
+            email_placeholder.style.zIndex = "6";
+            email_placeholder.style.fontSize = "1.2vw";
+            email_placeholder.style.transform = "translateY(100%)";
         })
-    }
-})
+    })
+
+    email.addEventListener("focusout", function() {
+        if (email.value.length == 0) {
+            email_placeholder.style.animation = "input-placeholder-animation-2 0.4s ease";
+
+            email_placeholder.addEventListener("animationend", function() {
+                email_placeholder.style.animation = '';
+                email_placeholder.style.top = "50%";
+                email_placeholder.style.zIndex = "5";
+                email_placeholder.style.fontSize = "2.5vw";
+                email_placeholder.style.transform = "translateY(-50%)";
+            })
+        }
+    })
+} else {
+    email.addEventListener("focus", function() {
+        email_placeholder.style.animation = "input-placeholder-animation 0.4s ease";
+
+        email_placeholder.addEventListener("animationend", function() {
+            email_placeholder.style.animation = '';
+            email_placeholder.style.top = "-100%";
+            email_placeholder.style.zIndex = "6";
+            email_placeholder.style.fontSize = "1.2vw";
+            email_placeholder.style.transform = "translateY(100%)";
+        })
+    })
+
+    email.addEventListener("focusout", function() {
+        if (email.value.length == 0) {
+            email_placeholder.style.animation = "input-placeholder-animation-2 0.4s ease";
+
+            email_placeholder.addEventListener("animationend", function() {
+                email_placeholder.style.animation = '';
+                email_placeholder.style.top = "50%";
+                email_placeholder.style.zIndex = "5";
+                email_placeholder.style.fontSize = "2.5vw";
+                email_placeholder.style.transform = "translateY(-50%)";
+            })
+        }
+    })
+}
 
 const password = document.getElementById("password");
 const password_placeholder = document.getElementById("password-placeholder");
