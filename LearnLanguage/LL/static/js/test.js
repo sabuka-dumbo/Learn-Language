e57c54025fc54cbd2befe7d426493e36ecce2d9a
Sabuka-dumbo <anonym.sas@proton.me>
2024-04-29 14:57:07 +0000

    Apr 29, 2024, 6:57 PM

--- LearnLanguage/LL/static/js/test.js
+++ LearnLanguage/LL/static/js/test.js
@@ -16,10 +16,19 @@ function start_test() {
 
 function start_test2() {
     const start_div = document.getElementById("start-div");
+    const listening_test_div = document.getElementById("test-variant-1");
+
     start_div.style.animation = "start_test2 1.5s ease";
 
     start_div.addEventListener("animationend", function() {
         start_div.style.animation = '';
         start_div.style.opacity = "0%";
+
+        listening_test_div.style.animation = "start_test 1.5s ease";
+        listening_test_div.style.display = "block";
+
+        listening_test_div.addEventListener("animationend", function() {
+            listening_test_div.style.animation = '';
+        })
     })
 }
