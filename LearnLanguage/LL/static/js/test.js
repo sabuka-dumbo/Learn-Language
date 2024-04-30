dcc872f0c7ec4345196f56f668c01bb57ecb460f
Sabuka-dumbo <anonym.sas@proton.me>
2024-04-30 14:56:14 +0000

    Apr 30, 2024, 6:56 PM

--- LearnLanguage/LL/static/js/test.js
+++ LearnLanguage/LL/static/js/test.js
@@ -3,7 +3,7 @@ const start_button = document.getElementById("start_button");
 if (window.location.pathname === "/listening_test/") {
     start_test();
 } else if (window.location.pathname === "/word_test/") {
-    start_test();
+    start_test2();
 }
 
 function start_test() {
@@ -19,6 +19,18 @@ function start_test() {
 }
 
 function start_test2() {
+    const start_div = document.getElementById("start-div2");
+
+    start_div.style.display = "block";
+    start_div.style.animation = "start_test 1.5s ease";
+
+    start_div.addEventListener("animationend", function() {
+        start_div.style.animation = '';
+        start_div.style.opacity = "100%";
+    })
+}
+
+function start_test3() {
     const start_div = document.getElementById("start-div");
     const listening_test_div = document.getElementById("test-variant-1");
 
@@ -37,7 +49,7 @@ function start_test2() {
     })
 }
 
-function start_test3() {
+function start_test4() {
     const start_div = document.getElementById("start-div");
     const word_test_div = document.getElementById("test-variant-2");
 
