2e4d1b4bfd7f417a78fe15779b4e4141e2783367
Sabuka-dumbo <anonym.sas@proton.me>
2024-05-04 13:07:31 +0000

    May 4, 2024, 5:07 PM

--- LearnLanguage/LL/static/js/index.js
+++ LearnLanguage/LL/static/js/index.js
@@ -25,14 +25,14 @@ function send_word() {
             const new_word_textarea = document.getElementById("textarea");
             const new_word_radio1 = document.getElementById("radio1");
             const new_word_check = document.getElementById("new-word-check");
-
+            
             new_word_div.style.animation = "fade_out 0.5s ease";
             new_word_check.style.animation = "fade_in 1s ease";
             new_word_check.style.display = "block";
-
+            
             new_word_check.addEventListener("animationend", function() {
                 new_word_check.style.animation = '';
-            })
+            });
             
             new_word_div.addEventListener("animationend", function() {
                 new_word_div.style.animation = '';
@@ -47,11 +47,11 @@ function send_word() {
                         new_word_check.style.animation = '';
                         new_word_check.style.display = "none";
                     });
-
+            
                     new_word_div.addEventListener("animationend", function() {
                         new_word_div.style.animation = '';
                         new_word_div.style.display = "block";
-                    })
+                    });
                 }, 1000);
             });
         })
