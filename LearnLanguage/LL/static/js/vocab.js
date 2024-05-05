6d69c1ffa3bb3eb0600b941901bc2276d8084c5d
Sabuka-dumbo <anonym.sas@proton.me>
2024-05-05 08:02:55 +0000

    May 5, 2024, 12:02 PM

--- LearnLanguage/LL/static/js/vocab.js
+++ LearnLanguage/LL/static/js/vocab.js
@@ -1,9 +1,11 @@
 function open_edit_div(word, meaning) {
     const edit_div = document.getElementById("edit-div");
+    const edit_header = document.getElementById("edit-header");
     const edit_input = document.getElementById("edit-list-input");
     const edit_input2 = document.getElementById("edit-list-input2");
 
     edit_div.style.display = "block";
     edit_input.placeholder = word;
     edit_input2.placeholder = meaning;
+    edit_header.innerHTML = `<b style="color: greenyellow;"> | </b>Edit the word: ${word}`
 }
