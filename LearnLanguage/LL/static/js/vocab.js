d4948103ea718a791c16d5d5967c2627490e6333
Sabuka-dumbo <anonym.sas@proton.me>
2024-05-05 08:16:11 +0000

    May 5, 2024, 12:16 PM

--- LearnLanguage/LL/static/js/vocab.js
+++ LearnLanguage/LL/static/js/vocab.js
@@ -3,21 +3,46 @@ const edit_back = document.getElementById("edit-back");
 const edit_header = document.getElementById("edit-header");
 const edit_input = document.getElementById("edit-list-input");
 const edit_input2 = document.getElementById("edit-list-input2");
+const edit_cancel = document.getElementById("edit-list-cancel");
+const edit_save = document.getElementById("edit-list-save");
 
 function open_edit_div(word, meaning) {
     edit_div.style.display = "block";
     edit_input.placeholder = word;
     edit_input2.placeholder = meaning;
     edit_header.innerHTML = `<b style="color: greenyellow;"> | </b>Edit the word: ${word}`;
+    edit_cancel.onclick = 'cacel_edit_div(';
+    edit_save.onclick = `save_edit_div(${word}, ${meaning})`;
 }
 
 edit_back.addEventListener("click", function() {
     edit_div.style.display = "none";
 })
 
-function c_edit_div(word, meaning) {
-    edit_div.style.display = "block";
-    edit_input.placeholder = word;
-    edit_input2.placeholder = meaning;
-    edit_header.innerHTML = `<b style="color: greenyellow;"> | </b>Edit the word: ${word}`;
+function cancel_edit_div(word, meaning) {
+    edit_div.style.display = "none";
+}
+
+function save_edit_div(word, meaning) {
+    edit_div.style.display = "none";
+    const warning_div = document.getElementById("warning");
+    const warning_text = document.getElementById("warning-text");
+    
+    warning_div.style.display = 'block';
+    warning_div.style.animation = 'warning-animation 1s ease';
+    warning_text.innerText = "Please fill in all the fields above";
+    
+    warning_div.addEventListener("animationend", function() {
+        warning_div.style.animation = '';
+        warning_div.style.display = "block";
+        
+        setTimeout(function() {
+            warning_div.style.animation = 'warning-animation2 2s ease';
+    
+            warning_div.addEventListener("animationend", function() {
+                warning_div.style.animation = '';
+                warning_div.style.display = "none";
+            });
+        }, 1500);
+    });
 }
